fdc78da666917716f2e51fd0bcdeb66a5f432517
dahs <hendra.dhonni.888@gmail.com>
2021-10-14 18:51:34 +0700

    complete footer and add animation on each section

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import Button from "components/button";
 import IconText from "./IconText";
 
@@ -8,37 +9,39 @@ export default function Header(props) {
   };
 
   return (
-    <header className="spacing-sm">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <IconText />
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <IconText />
 
-          <div className="collapse navbar-collapse flex-row-reverse">
-            <ul className="navbar-nav ml-auto">
-              <li className={`nav-item${getNavLinkClass("/")}`}>
-                <Button className="nav-link" type="link" href="">
-                  Home
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                <Button className="nav-link" type="link" href="/browse-by">
-                  Browse By
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/stories")}`}>
-                <Button className="nav-link" type="link" href="/stories">
-                  Stories
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/agents")}`}>
-                <Button className="nav-link" type="link" href="/agents">
-                  Agents
-                </Button>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+            <div className="collapse navbar-collapse flex-row-reverse">
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-item${getNavLinkClass("/")}`}>
+                  <Button className="nav-link" type="link" href="">
+                    Home
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                  <Button className="nav-link" type="link" href="/browse-by">
+                    Browse By
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                  <Button className="nav-link" type="link" href="/stories">
+                    Stories
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                  <Button className="nav-link" type="link" href="/agents">
+                    Agents
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </Fade>
   );
 }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,48 +1,57 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import Button from "components/button";
 
 export default function MostPicked(props) {
   return (
     <section className="container" ref={props.refMostPicked}>
-      <h4 className="mb-3">Most Picked</h4>
-      <div className="row">
-        <div className="container-grid">
-          {props.data.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}
-              >
-                <div className="card card-featured">
-                  <div className="tag">
-                    ${item.price}
-                    <span className="font-weight-light">per {item.unit}</span>
+      <Fade bottom>
+        <h4 className="mb-3">Most Picked</h4>
+        <div className="row">
+          <div className="container-grid">
+            {props.data.map((item, index) => {
+              return (
+                <Fade bottom delay={300 * index}>
+                  <div
+                    key={index}
+                    className={`item column-4 ${
+                      index === 0 ? "row-2" : "row-1"
+                    }`}
+                  >
+                    <div className="card card-featured">
+                      <div className="tag">
+                        ${item.price}
+                        <span className="font-weight-light">
+                          per {item.unit}
+                        </span>
+                      </div>
+                      <figure className="img-wrapper">
+                        <img
+                          src={item.imageUrl}
+                          alt={item.name}
+                          className="img-cover"
+                        />
+                      </figure>
+                      <div className="meta-wrapper">
+                        <Button
+                          type="link"
+                          href={`/properties/${item._id}`}
+                          className="stretched-link d-block text-white"
+                        >
+                          <h5>{item.name}</h5>
+                        </Button>
+                        <span>
+                          {item.city}, {item.country}
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                  <figure className="img-wrapper">
-                    <img
-                      src={item.imageUrl}
-                      alt={item.name}
-                      className="img-cover"
-                    />
-                  </figure>
-                  <div className="meta-wrapper">
-                    <Button
-                      type="link"
-                      href={`/properties/${item._id}`}
-                      className="stretched-link d-block text-white"
-                    >
-                      <h5>{item.name}</h5>
-                    </Button>
-                    <span>
-                      {item.city}, {item.country}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+                </Fade>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      </Fade>
     </section>
   );
 }
